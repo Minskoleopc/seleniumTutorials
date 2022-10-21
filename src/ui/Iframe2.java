ffd9973b5354f415737240a213bcaf49e7297aad
Minskoleopc <minskoleopc@gmail.com>
2022-10-21 06:01:59 +0530

    added Iframe WindowHandles

--- src/ui/Iframe2.java
+++ src/ui/Iframe2.java
@@ -9,19 +9,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Iframe {
+public class Iframe2 {
 
 	public static void main(String[] args) throws InterruptedException {
 		
 				System.setProperty("webdriver.chrome.driver",
 						"C:\\Users\\ankurjain\\Desktop\\Documents\\chromedriver_win32 (2)\\chromedriver.exe");
 				ChromeDriver driver= new ChromeDriver(); 
-				driver.get("https://webdriveruniversity.com/IFrame/index.html");
-				WebElement frame = driver.findElement(By.id("frame"));
-				driver.switchTo().frame(frame);
-				driver.findElement(By.cssSelector("a[href=\"index.html\"]")).click();
-				System.out.println(driver.getTitle());
-				driver.switchTo().parentFrame();
+				driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+				WebElement  e =driver.findElement(By.id("iframeResult"));
+				driver.switchTo().frame(e);
+				driver.findElement(By.cssSelector("body > button")).click();
+				Thread.sleep(2000);
+				System.out.println(driver.switchTo().alert().getText());
+				Thread.sleep(2000);
+				driver.switchTo().alert().accept();
 				
 				
 				
